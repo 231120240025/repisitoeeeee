d210457adb1b33d60506cb6caf4574ee53c198a4
8NEMO <accidentxxxx@gmail.com>
2024-11-24 08:10:46 +0500

    в корне.

--- src/main/java/searchengine/model/Page.java
+++ src/main/java/searchengine/model/Page.java
@@ -1,4 +1,33 @@
 package searchengine.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.persistence.Index;
+
+@Entity
+@Table(name = "page", indexes = {
+        @Index(name = "idx_path", columnList = "path") // Индекс на поле path
+})
+@Data
+@NoArgsConstructor
 public class Page {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "site_id", nullable = false)
+    private Site site; // Внешний ключ, связывающий с таблицей site
+
+    @Column(name = "path", nullable = false, columnDefinition = "TEXT")
+    private String path;
+
+    @Column(name = "code", nullable = false)
+    private int code;
+
+    @Column(name = "content", nullable = false, columnDefinition = "MEDIUMTEXT")
+    private String content;
 }
--- src/main/java/searchengine/model/Site.java
+++ src/main/java/searchengine/model/Site.java
@@ -1,4 +1,35 @@
 package searchengine.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "site")
+@Data
+@NoArgsConstructor
 public class Site {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+
+    @Column(name = "status", nullable = false, columnDefinition = "ENUM('INDEXING', 'INDEXED', 'FAILED')")
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @Column(name = "status_time", nullable = false)
+    private LocalDateTime statusTime;
+
+    @Column(name = "last_error", columnDefinition = "TEXT")
+    private String lastError;
+
+    @Column(name = "url", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String url;
+
+    @Column(name = "name", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String name;
 }
